c9256cd713e6735c8d4cccf8612b1c1572e5d22a
unknown <homeland007@naver.com>
2024-07-04 17:05:57 +0900

    6번까지 초안

--- team project1.py
+++ team project1.py
@@ -1,19 +1,46 @@
-# ----- 코드 정의 ------
 class Member:
-    # TODO : 코드 구현이 필요합니다.
+    def __init__(self, name, username, password):
+        self.name = name
+        self.username = username
+        self.password = password
+        pass
 
     def display(self):
-        # TODO : 코드 구현이 필요합니다.
-        pass
+        print(f"이름: {self.name}")
+        print(f"아이디: {self.username}")
+
+
+members = []
+members.append(Member("이상혁", "faker", "0000"))
+members.append(Member("김건부", "canyon", "0002"))
+members.append(Member("김혁규", "deft", "0001"))
+
+print("회원 이름")
+for member in members:
+    print(member.name)
 
 
 class Post:
-    # TODO : 코드 구현이 필요합니다.
-    pass
+    def __init__(self, title, content, author) :
+        self.title = title 
+        self.content = content
+        self.author = author
+        pass        
 
 
-# ----- 코드 실행 ------
-members = []
+
 posts = []
 
-# TODO : 코드 구현이 필요합니다.
+for member in members:
+
+    posts.append(Post(f"{member.name}의 첫 번째 글", "첫 번째 게시물 내용", member.username))
+    posts.append(Post(f"{member.name}의 두 번째 글", "두 번째 게시물 내용", member.username))
+    posts.append(Post(f"{member.name}의 세 번째 글", "세 번째 게시물 내용", member.username))
+
+for post in posts:
+    print(post.title, post.content, post.author)   
+
+for post in posts:
+    if "두 번째" in post.content:
+        print(post.title)
+
