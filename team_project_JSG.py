357388a9d3e58aece5844c9de17fedac4a748bbd
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-07-04 20:15:54 +0900

    상속해서 만들어보려했지만, 차라리 author=user_name 선언하여 상속없이 진행

--- team_project_JSG.py
+++ team_project_JSG.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 members = [] #회원정보 보관 리스트
 posts = [] #post 보관 리스트
-class member:
+class Member:
     def __init__(self, name, user_name, password):
         self.name = name
         self.user_name = user_name
@@ -16,17 +16,22 @@ class member:
     def register(self):
         members.append(self.format_register())
         #for_register에 작성된 포맷으로 members리스트에 추가하기
-m1 = member('정순겸','gyum9779',1234) #정보저장을 위한 필요 데이터 입력
+
+m1 = Member('이상혁','faker',1234) 
+m2 = Member('이민석', 'keria',5678)
+m3 = Member('최우제','Zeus',9101)
+        #정보저장을 위한 필요 데이터 입력
 m1.display()
 m1.register()
+m2.register()
+m3.register()
 pprint(members)
 # title, content, author
-class Post(member):
+class Post():
     def __init__(self,title, content,user_name):
-        super().__init__(user_name)
         self.title = title
         self.content = content
-        self.author =  self.user_name
+        self.author =  user_name
         # Post의 author를 user_name으로
     def post_display(self):
         print(f'제목 : {self.title}')
@@ -38,3 +43,11 @@ class Post(member):
     def p_register(self):
         posts.append(self.p_format_register())
         # 코드실행
+m1_1 = Post('전적','1337전 907승 430패, 승률 67.8%, 총 킬 4725, 총 데스 2845, 총 어시스트 7314, KDA 4.3, 킬 관여율 64.9%','faker')
+m1_2 = Post('대회 경력','월즈 우승 4회, MSI 우승 2회, LCK 우승 10회, IEM 우승 1회, 리프트 라이벌즈 우승 1회, 리그 오브 레전드 올스타전 우승 5회, NLB 우승 1회, 아시안 게임 금메달','faker')
+m1_3 = Post('개인 업적','Worlds MVP 1회, MSI MVP 1회, LCK 통합 MVP 2회, LCK 포스트시즌 MVP 1회, LCK All-Pro First team 2회','faker')
+m1_1.post_display()
+m1_1.p_register()
+m1_2.p_register()
+m1_3.p_register()
+pprint(posts)
