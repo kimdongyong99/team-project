4455b19fd1c6456ab6efdd5595f40785e67fcd85
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-07-05 12:03:52 +0900

    input이용하여 정보 받기

--- team_project_JSG.py
+++ team_project_JSG.py
@@ -18,14 +18,14 @@ class Member:
         #for_register에 작성된 포맷으로 members리스트에 추가하기
 
 m1 = Member('이상혁','faker',1234) 
-m2 = Member('이민석', 'keria',5678)
+m2 = Member('류민석', 'keria',5678)
 m3 = Member('최우제','Zeus',9101)
-        #정보저장을 위한 필요 데이터 입력
-m1.display()
-m1.register()
-m2.register()
-m3.register()
-pprint(members)
+#         #정보저장을 위한 필요 데이터 입력
+# m1.display()
+# m1.register()
+# m2.register()
+# m3.register()
+# pprint(members)
 # title, content, author
 class Post:
     def __init__(self,title, content,user_name):
@@ -46,8 +46,32 @@ class Post:
 m1_1 = Post('전적','1337전 907승 430패, 승률 67.8%, 총 킬 4725, 총 데스 2845, 총 어시스트 7314, KDA 4.3, 킬 관여율 64.9%','faker')
 m1_2 = Post('대회 경력','월즈 우승 4회, MSI 우승 2회, LCK 우승 10회, IEM 우승 1회, 리프트 라이벌즈 우승 1회, 리그 오브 레전드 올스타전 우승 5회, NLB 우승 1회, 아시안 게임 금메달','faker')
 m1_3 = Post('개인 업적','Worlds MVP 1회, MSI MVP 1회, LCK 통합 MVP 2회, LCK 포스트시즌 MVP 1회, LCK All-Pro First team 2회','faker')
-m1_1.post_display()
-m1_1.p_register()
-m1_2.p_register()
-m1_3.p_register()
-pprint(posts)
+m2_2 = Post('전적',)
+#         # Post 작성
+# m1_1.post_display()
+# m1_1.p_register()
+# m1_2.p_register()
+# m1_3.p_register()
+# pprint(posts)
+cnt = 0
+def use_input():
+    while True:
+        a = int(input("1 또는 2를 입력하세요(회원등록: 1, 게시글 작성: 2"))
+        if (a !=1) or (a !=2):
+            print("다시 입력하세요. 회원등록: 1, 게시글: 2")
+            continue
+        if a == 1:
+            mem_name = input("이름을 입력하세요")
+            mem_user_name = input("UserName을 입력하세요")
+            mem_password = input("비밀번호를 입력하세요")
+            mem = Member(mem_name,mem_user_name,mem_password)
+            mem.register()
+            break
+        elif a == 2:
+            for_post_title = input("제목을 입력하세요")
+            for_post_content = input("내용을 입력하세요")
+            for_post_author = input("UserName을 입력하세요")
+            for_post = Post(for_post_title,for_post_content,for_post_author)
+            for_post.p_register()
+            break
+use_input()
