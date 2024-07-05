780a3825cabd928c47a73972fb9a5ff8f3cda5b7
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-07-05 16:14:09 +0900

    원하는 출력 (print문) 변경

--- team_project_JSG.py
+++ team_project_JSG.py
@@ -19,7 +19,7 @@ class Member:
 
     def format_register(self):
         for_hashed_pw = self.password + salt
-        hashed_pw = hashlib.sha256(for_hashed_pw.encode()).hexdigest() 
+        hashed_pw = hashlib.sha256(for_hashed_pw.encode()).hexdigest()
         #비밀번호 해싱
         return {'이름': self.name, '유저네임': self.user_name, '비밀번호': hashed_pw}
         #해싱된 비밀번호로 저장
@@ -30,11 +30,12 @@ class Member:
         # for_register에 작성된 포맷으로 members리스트에 추가하기
 
 
-m1 = Member('이상혁', 'faker', 1234)
-m2 = Member('류민석', 'keria', 5678)
-m3 = Member('최우제', 'Zeus', 9101)
+# m1 = Member('이상혁', 'faker', 1234)
+# m2 = Member('류민석', 'keria', 5678)
+# m3 = Member('최우제', 'zeus', 91011)
 
-
+for i in members:
+    print(members[1]['name'])
 #         #정보저장을 위한 필요 데이터 입력
 # m1.display()
 # m1.register()
@@ -63,23 +64,33 @@ class Post:
 
 #         # 코드실행
 # m1_1 = Post('전적','1337전 907승 430패, 승률 67.8%, 총 킬 4725, 총 데스 2845, 총 어시스트 7314, KDA 4.3, 킬 관여율 64.9%','faker')
-# m1_2 = Post('대회 경력','월즈 우승 4회, MSI 우승 2회, LCK 우승 10회, IEM 우승 1회, 리프트 라이벌즈 우승 1회, 리그 오브 레전드 올스타전 우승 5회, NLB 우승 1회, 아시안 게임 금메달','faker')
-# m1_3 = Post('개인 업적','Worlds MVP 1회, MSI MVP 1회, LCK 통합 MVP 2회, LCK 포스트시즌 MVP 1회, LCK All-Pro First team 2회','faker')
-# m2_2 = Post('전적',)
+# m1_2 = Post('MBTI','ENFP','faker')
+# m1_3 = Post('솔랭 닉네임','hide on bush. 일명 하온부','faker')
+# m2_1 = Post('별명','역천괴 - 역대급 천재 괴물, 울한울 -울어..한없이 울어..','keria' )
+# m2_2 = Post('MBTI','ISFP','keria' )
+# m2_3 = Post('업적','2022년 1월 1일, 페이커와의 인스타그램 맞팔로우에 성공','keria' )
+# m3_1 = Post('오트와 우트','패트와 매트에서 따온, 오너와 함께 불리는 듀오 별명','zeus')
+# m3_2 = Post('MBTI','INFP','zeus')
+# m3_3 = Post('생년월일','2004년 1월 31일','zeus')
 #         # Post 작성
 # m1_1.post_display()
 # m1_1.p_register()
 # m1_2.p_register()
 # m1_3.p_register()
+# m2_1.p_register()
+# m2_2.p_register()
+# m2_3.p_register()
+# m3_1.p_register()
+# m3_2.p_register()
+# m3_3.p_register()
 # pprint(posts)
-cnt = 0
 
 
 def use_input():
     while True:
         a = int(input("숫자를 입력하세요(회원등록: 1, 게시글 작성: 2, 회원리스트: 3, 게시글리스트: 4, 종료: 5): "))
         if (a != 1) and (a != 2) and (a != 3) and (a != 4) and (a != 5):
-            print("다시 입력하세요. (회원등록: 1, 게시글: 2):")
+            print("숫자를 다시 입력하세요. (회원등록: 1, 게시글: 2, 회원리스트: 3, 게시글리스트: 4, 종료: 5):")
             continue
         if a == 1:
             mem_name = input("이름을 입력하세요: ")
@@ -94,9 +105,24 @@ def use_input():
             for_post = Post(for_post_title, for_post_content, for_post_author)
             for_post.p_register()
         elif a == 3:
-            pprint(members)
+            if not members:
+                print("등록된 회원이 없습니다")
+                #비어있는 리스트의 bool 값은 false
+            else:
+                for i in members:
+                    i_name = i['이름']
+                    i_user_name = i['유저네임']
+                    print(f'이름: {i_name}  유저네임: {i_user_name}')
+                    # print(f'이름: {i['이름']}  유저네임: {i['유저네임']}')
+                    #비밀번호 표시 x
         elif a == 4:
-            pprint(posts)
+            if not posts:
+                print("등록된 게시물이 없습니다")
+            else:
+                for j in posts:
+                    j_author = j['글쓴이']
+                    j_title = j['제목']
+                    print(f'작성자: {j_author}  제목: {j_title}')
         elif a == 5:
             break
 
